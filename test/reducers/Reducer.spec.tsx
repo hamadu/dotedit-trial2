d59b562361917e29c81eb139f009f6a4c287dce5
hamadu <dercll1020@gmail.com>
2016-11-21 01:22:21 +0900

    test frameworks(ongoing)

---
+++ test/reducers/Reducer.spec.tsx
@@ -0,0 +1,11 @@
+import { reduce } from "../../src/js/reducers/Reducer";
+
+describe("Reducer", () => {
+  beforeEach(() => {
+  });
+
+  it("testing test", () => {
+      const result = 1+2;
+      chai.assert.strictEqual(result, 3);
+  });
+});
